0fa8888344541965a3ef7e28a6a709e2efcf2758
Anna <annalordkemp@gmail.com>
2022-12-19 13:59:52 -0700

    halfway through wave 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import React, { useState } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import axios from 'axios';
 
-const TASKS_DATA = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+const kBaseUrl = 'https://task-list-api-c17.herokuapp.com';
+
+const convertFromApiFormat = (apiTask) => {
+  const { is_complete:isComplete , id, description, title } = apiTask;
+  const newTask = { isComplete, id, description, title }; 
+  return newTask;
+};
+
+const getAllTasksApi = () => {
+  return axios.get(`${kBaseUrl}/tasks`)
+  .then(response => {
+    return response.data.map(convertFromApiFormat);
+  })
+  .catch(err => console.log(err));
+};
+
+const markCompleteApi = (id) => {
+  return axios.patch(`${kBaseUrl}/tasks/${id}/mark_complete`)
+  .then (response => {
+    return convertFromApiFormat(response.data);
+  })
+  .catch (err => console.log(err));
+};
+
+const markIncompleteApi = (id) => {
+  return axios.patch(`${kBaseUrl}/tasks/${id}/mark_incomplete`)
+  .then (response => {
+    return convertFromApiFormat(response.data);
+  })
+  .catch (err => console.log(err));
+};
+
+const deleteTaskApi = (id) => {
+  return axios.delete(`${kBaseUrl}/tasks/${id}`)
+  .then (response => {
+    return convertFromApiFormat(response.data);
+  })
+  .catch (err => console.log(err));
+};
 
 const App = () => {
 
-  const [tasks, setTasks] = useState(TASKS_DATA);
+  const [tasks, setTasks] = useState([]);
 
   const setComplete = (id) => {
     setTasks(tasks => tasks.map(task => { 
